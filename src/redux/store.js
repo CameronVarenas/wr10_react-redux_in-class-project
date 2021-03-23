e3719d066ecc2bb3eec97b6fbddc8f6ad89a389f
Cameron Varenas <camerontvarenas@gmail.com>
2021-03-23 16:05:19 -0400

    Finished lecture code

---
+++ src/redux/store.js
@@ -0,0 +1,24 @@
+// If multiple reducers
+
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+import userReducer from './userReducer';
+import productsReducer from './productsReducer';
+
+const rootReducer = combineReducers({
+    userReducer,
+    productsReducer
+})
+
+export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
+
+
+
+
+
+// If only 1 reducer
+
+// import {createStore, combineReducers} from 'redux';
+// import userReducer from './userReducer'
+
+// export default createStore(userReducer);
---
+++ src/redux/userReducer.js
@@ -0,0 +1,42 @@
+// 4 parts of the reducer filer: initial state, action types, action creators, reducer function
+
+// Initial State
+const initialState = {
+    username: '',
+    isLoggedIn: false,
+
+}
+
+// Action Types
+const LOGIN_USER = 'LOGIN_USER';
+const LOGOUT_USER = 'LOGOUT_USER';
+
+// Action Creators
+export function loginUser(usernameStr) {
+    return {
+        type: LOGIN_USER,
+        payload: usernameStr
+    }
+}
+
+export function logoutUser() {
+    return {
+        type: LOGOUT_USER
+    }
+}
+
+
+// Reducer
+export default function reducer(state = initialState, action) {
+    switch(action.type) {
+        case LOGIN_USER:
+            return {
+                ...state,
+                username: action.payload,
+                isLoggedIn: true,
+            }
+        case LOGOUT_USER:
+            return initialState;
+        default: return state;
+    }
+}
